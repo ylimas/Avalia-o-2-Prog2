480d15b4f51f61eb3d474beb692007ef8a491e30
ylimas <yagovianaalencar23@gmail.com>
2025-11-12 21:57:27 -0300

    Create servidor_srp.py

---
+++ servidor_srp.py
@@ -0,0 +1,28 @@
+from xmlrpc.server import SimpleXMLRPCServer
+import tadbd_srp.py
+
+#definindo as funções
+
+
+
+def adPassageiro(idpass, nome, email, fone):
+	pass
+def adReserva(idres, data, status, assento, idpass, idvoo):
+	pass
+def adVoo(idvoo, numvoo, origem, destino, dtpartida, dtchegada):
+	pass
+def getPassageiro(id):
+	pass
+def getVoo(id):
+	pass
+def getReserva(id):
+	pass
+def vooExiste(idvoo):
+	pass
+def passExiste(idpass):
+	pass
+def vagasVoo(idvoo):
+	pass
+def assentoLivre(idvoo, assento):
+	pass
+	
