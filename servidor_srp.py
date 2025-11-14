82acd38360ec0b1da662cf1f38caad5f108187a7
ylimas <yagovianaalencar23@gmail.com>
2025-11-14 09:25:39 -0300

    VERSÃO FINAL

--- servidor_srp.py
+++ servidor_srp.py
@@ -1,29 +1,29 @@
 from xmlrpc.server import SimpleXMLRPCServer
-import tadbd_srp.py as tadbd
-
-#definindo as funções
-
-
+import tadbd_srp as tadbd
 
 def adPassageiro(idpass, nome, email, fone):
-	tadbd.adPassageiro(bd, idpass, nome, email, fone)
+	return tadbd.adPassageiro(bd, idpass, nome, email, fone)
 
 def adReserva(idres, data, status, assento, idpass, idvoo):
-	tadbd.adReserva(bd, idres, bd['voos'][idvoo]['dataPda'], 'confirmada', assento, idpass, idvoo)
+	return tadbd.adReserva(bd, idres, data, status, assento, idpass, idvoo)
 
 def adVoo(idvoo, numvoo, origem, destino, dtpartida, dtchegada):
-	tadbd.adVoo(bd, idvoo, numvoo, origem, destino, dtpartida, dtchegada)
+	return tadbd.adVoo(bd, idvoo, numvoo, origem, destino, dtpartida, dtchegada)
 
 def getPassageiro(id):
 	if tadbd.passExiste(bd, id) == True:
 		return bd['passageiros'][id]
+	return None
 
 def getVoo(id):
 	if tadbd.vooExiste(bd, id) == True:
 		return bd['voos'][id]
+	return None
 
 def getReserva(id):
-	return bd['reservas'][id]
+	if id in bd['reservas']:
+		return bd['reservas'][id]
+	return None
 
 def vooExiste(idvoo):
 	return tadbd.vooExiste(bd, idvoo)
@@ -37,36 +37,26 @@ def vagasVoo(idvoo):
 def assentoLivre(idvoo, assento):
 	return tadbd.assentoLivre(bd, idvoo, assento)
 	
-#--------------------MAIN---------------------
-
-
 def main():
 	print("Servidor SRP rodando na porta Local 8000...")
 	server = SimpleXMLRPCServer(("localhost", 8000))
 
-
 	global bd
 	bd = tadbd.open_bd('tabpassageiros.txt', 'tabreservas.txt', 'tabvoos.txt')
 
-
-#-------------REGISTRANDO---AS---FUNÇÕES-------------
-
-	server.register_function(adPassageiro, 'adPass')
-	server.register_function(adReserva, 'adRes')
+	server.register_function(adPassageiro, 'adPassageiro')
+	server.register_function(adReserva, 'adReserva')
 	server.register_function(adVoo, 'adVoo')
-	server.register_function(getPassageiro, 'getPass')
+	server.register_function(getPassageiro, 'getPassageiro')
 	server.register_function(getVoo, 'getVoo')
-	server.register_function(getReserva, 'getRes')
+	server.register_function(getReserva, 'getReserva')
 	server.register_function(vooExiste, 'vooExiste')
 	server.register_function(passExiste, 'passExiste')
-	server.register_function(vagasVoo, 'vagas')
+	server.register_function(vagasVoo, 'vagasVoo')
 	server.register_function(assentoLivre, 'assentoLivre')
 
-#---------RODANDO----SERVIDOR-------------------
 	server.serve_forever()
 
-
-#-------SALVA ANTES DE ENCERRAR----------------
 	tadbd.salva_bd(bd, 'tabpassageiros.txt', 'tabreservas.txt', 'tabvoos.txt')
 
 main()
